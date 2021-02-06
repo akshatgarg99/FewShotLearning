7bb0c0143b443b05ba5ce1e62acfcafea33f6176
Akshat Garg <gargakshat99@gmail.com>
2021-02-06 14:28:17 +0530

    added model

---
+++ Models/model.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+
+
+def conv_block(out_channels):
+    return nn.Sequential(
+        nn.Conv2d(3, out_channels, 3, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2)
+    )
+
+
+class Classifier(nn.Module):
+    def __init__(self, num_input_channels):
+        super(Classifier,self).__init__()
+        self.conv1 = conv_block(num_input_channels, 64)
+        self.conv2 = conv_block(64, 64)
+        self.conv3 = conv_block(64, 64)
+        self.conv4 = conv_block(64, 64)
+        self.conv5 = conv_block(64,64)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+
+        x = x.view(x.size(0), -1)
+        return x
